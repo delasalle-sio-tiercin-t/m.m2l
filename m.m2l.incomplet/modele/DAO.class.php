8a681eb12fdf914957deeecdd4e4b99fc7e14a89
delasalle-sio-Ewan <delasalle.sio.penanchaot.e@gmail.com>
2016-10-04 15:27:23 +0200

    Création fonction existeReservation Modification Excel

--- m.m2l.incomplet/modele/DAO.class.php
+++ m.m2l.incomplet/modele/DAO.class.php
@@ -70,18 +70,6 @@ class DAO
 	public function __destruct() {
 		unset($this->cnx);
 	}
-	
-	public function estLeCreateur($nomUser, $idReservation) {
-		$crea = "Select create_by, id from mrbs_entry  where create_by = ':create_by AND id= ':id'";
-		$crea->bindValue("create_by", $nomUser->create_by, PDO::PARAM_INT);
-		$crea->bindValue("id", $idReservation->id, PDO::PARAM_INT);
-		$req = $this->cnx->prepare($crea);
-		$ok = $req->execute();
-		
-		if (!empty($ok))
-			return true;
-		else return false;
-	}
 
 	// ------------------------------------------------------------------------------------------------------
 	// -------------------------------------- Méthodes d'instances ------------------------------------------
@@ -99,13 +87,12 @@ class DAO
 	}
 
 	public function getReservation($idReservation) {
-		$getres = "Select * from mrbs_entry where id = ':id'";
+		$getres = "Select id from mrbs_entry where id = ':id'";
 		$getres->bindValue("id", $idReservation->id, PDO::PARAM_INT);
 		$req1 = $this->cnx->prepare($getres);
-		$req2 = $req->execute();
 		
-		if (!empty(req2))
-			return $req2;
+		if (empty(req1))
+			return $req1;
 		else 
 			return null;
 			
@@ -215,27 +202,6 @@ class DAO
 		else
 			return true;
 	}
-	
-	// fournit true si l'utilisateur ($idReservation) existe, false sinon
-	// modifié par Legrand le 04/10/2016
-	public function existeReservation($idReservation)
-	{	// préparation de la requête de recherche
-		$txt_req = "SELECT COUNT(*) FROM mrbs WHERE id = :idReservation";
-		$req = $this->cnx->prepare($txt_req);
-		//liaison de la requête et de ses paramètres
-		$req->bindValue("idReservation",$idReservation,PDO::PARAM_INT);
-		//exécution de la requête 
-		$req->execute();
-		$nbReponses = $req->fetchColumn(0);
-		// libère les ressources du jeu de données
-		$req->closeCursor();
-		
-		// fourniture de la réponse
-		if ($nbReponses == 0)
-			return false;
-			else
-				return true;
-		}
 
 	// génération aléatoire d'un digicode de 6 caractères hexadécimaux
 	// modifié par Jim le 5/5/2015
@@ -395,6 +361,27 @@ public function getLesSalles() {
 	
 		
 	}
+	
+	public function existeReservation($idReservation)
+	{	// préparation de la requete de recherche
+	$txt_req = "Select id from mrbs_entry where id = :idReservation";
+	$req = $this->cnx->prepare($txt_req);
+	// liaison de la requête et de ses paramètres
+	$req->bindValue("idReservation", $idReservation, PDO::PARAM_STR);
+	// exécution de la requete
+	$req->execute();
+	$existeRes = $req->fetchColumn(0);
+	// libère les ressources du jeu de données
+	$req->closeCursor();
+	
+	// fourniture de la réponse
+	if ($existeRes == 0)
+		return false;
+		else
+			return true;
+	}
+	
+	
 	
 	
 	
