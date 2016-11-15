4be3d69345198dcb8d676bb5784e72a56b277cc1
delasalle-sio-legrand-m <delasalle.sio.legrand.m@gmail.com>
2016-11-15 17:17:55 +0100

    AnnulerReservation fait

--- m.m2l.incomplet/vues/VueAnnulerReservation.php
+++ m.m2l.incomplet/vues/VueAnnulerReservation.php
@@ -1,36 +1,61 @@
+<?php
+	// Projet Réservations M2L - version web mobile
+	// fichier : vues/VueCreerUtilisateur.php
+	// Rôle : visualiser la demande de création d'un nouvel utilisateur
+	// cette vue est appelée par le contôleur controleurs/CtrlCreerUtilisateur.php
+	// Création : 12/10/2015 par JM CARTRON
+	// Mise à jour : 2/6/2016 par JM CARTRON
+?>
 <!doctype html>
 <html>
 	<head>
 		<?php include_once ('vues/head.php'); ?>
+		
+		<script>
+			// associe une fonction à l'événement pageinit
+			$(document).bind('pageinit', function() {
+				<?php if ($typeMessage != '') { ?>
+					// affiche la boîte de dialogue 'affichage_message'
+					$.mobile.changePage('#affichage_message', {transition: "<?php echo $transition; ?>"});
+				<?php } ?>
+			} );
+		</script>
 	</head>
-	 
+	
 	<body>
-		<div data-role="page">
+		<div data-role="page" id="page_principale">
 			<div data-role="header" data-theme="<?php echo $themeNormal; ?>">
 				<h4>M2L-GRR</h4>
 				<a href="index.php?action=Menu" data-transition="<?php echo $transition; ?>">Retour menu</a>
 			</div>
+			
 			<div data-role="content">
-				<h4 style="text-align: center; margin-top: 0px; margin-bottom: 0px;">Consulter mes réservations</h4>
-				<p style="text-align: center;"><?php echo $message; ?></p>
-				<ul data-role="listview" style="margin-top: 5px;">
-				
-				<form action="index.php?action=AnnulerReservation" method="post">
+				<h4 style="text-align: center; margin-top: 0px; margin-bottom: 0px;">Confirmer une réservation</h4>
+				<form action="index.php?action=AnnulerReservation" method="post" data-ajax="false">
 					<div data-role="fieldcontain" class="ui-hide-label">
-						<input type="text" name="idRes" id="idRes" required placeholder="Entrez le numéro de la réservation" value="<?php echo $idReservation; ?>">
+						<label for="txtIdReservation">Numéro de réservation :</label>
+						<input type="text" name="txtIdReservation" id="txtIdReservation" required placeholder="Entrez le numéro de la réservation..." value="<?php echo $idReservation; ?>">
 					</div>
 					<div data-role="fieldcontain">
-						<input type="submit" name="btnSupprimerReservation" id="btnSupprimerReservation" value="Supprimer la réservation" data-mini="true">
+						<input type="submit" name="btnAnnulerReservation" id="btnAnnulerReservation" value="Annuler la réservation" data-mini="true">
 					</div>
 				</form>
-	
 
-				</ul>
+				<?php if($debug == true) {
+					// en mise au point, on peut afficher certaines variables dans la page
+					echo "<p>name = " . $name . "</p>";
+					echo "<p>adrMail = " . $adrMail . "</p>";
+					echo "<p>level = " . $level . "</p>";
+				} ?>
+				
 			</div>
-			<div data-role="footer" data-position="fixed" data-theme="<?php echo $themeNormal;?>">
+			
+			<div data-role="footer" data-position="fixed" data-theme="<?php echo $themeNormal; ?>">
 				<h4>Suivi des réservations de salles<br>Maison des ligues de Lorraine (M2L)</h4>
 			</div>
 		</div>
+		
+		<?php include_once ('vues/dialog_message.php'); ?>
 		
 	</body>
 </html>
