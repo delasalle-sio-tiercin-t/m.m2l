114d793fad4a01dea28b5102dd0a58926e3a7929
delasalle-sio-Ewan <delasalle.sio.penanchaot.e@gmail.com>
2016-11-15 16:04:17 +0100

    p

--- m.m2l.incomplet/controleurs/CtrlChangerDeMdp.php
+++ m.m2l.incomplet/controleurs/CtrlChangerDeMdp.php
@@ -6,12 +6,7 @@
 // modifié par Jim le 28/6/2016
 
 // on vérifie si le demandeur de cette action est bien authentifié
-if ( $_SESSION['niveauUtilisateur'] != 'utilisateur' && $_SESSION['niveauUtilisateur'] != 'administrateur') {
-	// si le demandeur n'est pas authentifié, il s'agit d'une tentative d'accès frauduleux
-	// dans ce cas, on provoque une redirection vers la page de connexion
-	header ("Location: index.php?action=Deconnecter");
-}
-else {
+
 	// connexion du serveur web à la base MySQL
 	include_once ('modele/DAO.class.php');
 	$dao = new DAO();
@@ -64,6 +59,6 @@ else {
 	unset($dao);		// fermeture de la connexion à MySQL
 }
 }
-}
+
 
 ?>
