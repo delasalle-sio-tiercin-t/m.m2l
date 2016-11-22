63b1d82d59a11c1060ab7cef02d01b169999e474
delasalle-sio-legrand-m <delasalle.sio.legrand.m@gmail.com>
2016-11-22 17:19:20 +0100

    Service web ConfirmerReservation

--- m.m2l.incomplet/services/ConfirmerReservation.php
+++ m.m2l.incomplet/services/ConfirmerReservation.php
@@ -1,7 +1,7 @@
 <?php
 // Service web du projet Réservations M2L
-// Ecrit le 21/5/2015 par Jim
-// Modifié le 2/6/2016 par Jim
+// Ecrit le 22/11/2016 par LEGRAND Mathieu
+// Modifié le 2/6/2016 par LEGRAND Mathieu
 
 // Ce service web permet à un administrateur authentifié d'enregistrer un nouvel utilisateur
 // et fournit un compte-rendu d'exécution
@@ -20,68 +20,64 @@ include_once ('../modele/parametres.localhost.php');
 
 // Récupération des données transmises
 // la fonction $_GET récupère une donnée passée en paramètre dans l'URL par la méthode GET
-if ( empty ($_GET ["nomAdmin"]) == true)  $nomAdmin = "";  else   $nomAdmin = $_GET ["nomAdmin"];
-if ( empty ($_GET ["mdpAdmin"]) == true)  $mdpAdmin = "";  else   $mdpAdmin = $_GET ["mdpAdmin"];
-if ( empty ($_GET ["name"]) == true)  $name = "";  else   $name = $_GET ["name"];
-if ( empty ($_GET ["level"]) == true)  $level = "";  else   $level = $_GET ["level"];
-if ( empty ($_GET ["email"]) == true)  $email = "";  else   $email = $_GET ["email"];
+if ( empty ($_GET ["nom"]) == true)  $nom = "";  else   $nom = $_GET ["nom"];
+if ( empty ($_GET ["mdp"]) == true)  $mdp = "";  else   $mdp = $_GET ["mdp"];
+if ( empty ($_GET ["numreservation"]) == true)  $numReservation = "";  else   $numReservation = $_GET ["numreservation"];
+
 
 // si l'URL ne contient pas les données, on regarde si elles ont été envoyées par la méthode POST
 // la fonction $_POST récupère une donnée envoyées par la méthode POST
-if ( $nomAdmin == "" && $mdpAdmin == "" && $name == "" && $level == "" && $email == "" ) {
-	if ( empty ($_POST ["nomAdmin"]) == true)  $nomAdmin = "";  else   $nomAdmin = $_POST ["nomAdmin"];
-	if ( empty ($_POST ["mdpAdmin"]) == true)  $mdpAdmin = "";  else   $mdpAdmin = $_POST ["mdpAdmin"];
-	if ( empty ($_POST ["name"]) == true)  $name = "";  else   $name = $_POST ["name"];
-	if ( empty ($_POST ["level"]) == true)  $level = "";  else   $level = $_POST ["level"];
-	if ( empty ($_POST ["email"]) == true)  $email = "";  else   $email = $_POST ["email"];
+if ( $nom == "" && $mdp == "" && $numReservation == "") {
+	if ( empty ($_GET ["nom"]) == true)  $nom = "";  else   $nom = $_GET ["nom"];
+if ( empty ($_GET ["mdp"]) == true)  $mdp = "";  else   $mdp = $_GET ["mdp"];
+if ( empty ($_GET ["numreservation"]) == true)  $numReservation = "";  else   $numReservation = $_GET ["numerservation"];
 }
 
 // Contrôle de la présence des paramètres
-if ( $nomAdmin == "" || $mdpAdmin == "" || $name == "" || $level == "" || $email == "" || Outils::estUneAdrMailValide ($email) == false ) {
+if ( $nom == "" || $mdp == "" || $numReservation == "") {
 	$msg = "Erreur : données incomplètes ou incorrectes.";
 }
-else {
-	if ( $level != "0" && $level != "1" && $level != "2" ) {
-		$msg = "Erreur : le niveau doit être 0, 1 ou 2.";
-	}
 	else {
 		// connexion du serveur web à la base MySQL ("include_once" peut être remplacé par "require_once")
 		include_once ('../modele/DAO.class.php');
 		$dao = new DAO();
 
-		if ( $dao->getNiveauUtilisateur($nomAdmin, $mdpAdmin) != "administrateur" ) {
+		if (! $dao->existeUtilisateur($nom) || !($dao->getNiveauUtilisateur($nom,$mdp) != "0" && $dao->getNiveauUtilisateur($nom,$mdp) != "1" && $dao->getNiveauUtilisateur($nom,$mdp) != "2")) 
+		{
 			$msg = "Erreur : authentification incorrecte.";
 		}
 		else
 		{
-			if (! $dao->existeReservation($idReservation) ) {
+			if (! $dao->existeReservation($numReservation) ) {
 				$msg = "Erreur : la réservation n'existe pas !";
 			}
 			else {
 				// On vérifie si la personne est bien l'auteur de la réservation
-				$ok = $dao->estLeCreateur($name,$idReservation);
+				$ok = $dao->estLeCreateur($nom,$numReservation);
 				if ( ! $ok ) {
 					$msg = "Erreur : vous n'êtes pas l'auteur de cette réservation !";
 				}
 				else {
 					//on vérifie si la réservation a déjà été confirmée
-					$reservation = $dao->getReservation($idReservation);
+					$reservation = $dao->getReservation($numReservation);
 					$status = $reservation->getStatus();
-					if ($status = 0) {
+					if ($status == 0) {
 						$msg = "Erreur : cette réservation a déjà été confirmée !";
 					}
 					else {
 					// on vérifie si la réservation est déjà passée 
 					$date = $reservation->getEnd_time();
-					$timestamp = strtotime($date);
-						if ($timestamp < time()){
+						if ($date < time()){
 								$msg = "Erreur : cette réservation est déjà passée !";
 						}
 						else 
-						{
+						{	
+							$dao->confirmerReservation($numReservation);
+							$unUtilisateur = $dao->getUtilisateur($nom);
+							$email = $unUtilisateur->getEmail();
 							// envoi d'un mail de confirmation de l'enregistrement
-							$sujet = "Confirmation de la réservation numéro " . $idReservation;
-							$contenuMail = "La réservation numéro ." . $idReservation . " a été confirmée. \n\n";
+							$sujet = "Confirmation de la réservation numéro " . $numReservation;
+							$contenuMail = "La réservation numéro ." . $numReservation . " a été confirmée. \n\n";
 						
 							$ok = Outils::envoyerMail($email, $sujet, $contenuMail, $ADR_MAIL_EMETTEUR);
 							if ( ! $ok ) {
@@ -100,7 +96,6 @@ else {
 		// ferme la connexion à MySQL :
 		unset($dao);
 	}
-}
 
 // création du flux XML en sortie
 creerFluxXML ($msg);
